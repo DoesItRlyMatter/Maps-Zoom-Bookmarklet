c6f8a9d2d8343ad2e8f34ae8bb116017c7d14a87
dirm <anton.kerke@gmail.com>
2020-03-16 20:36:27 +0200

    basic functionality

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Maps-Zoom-Bookmarklet
 Bookmarklet that zooms in google maps to specified value.
+
+Note: bookmarklet will not work after search before interacting with map, might or might not fix in future.
---
+++ maps_zoom_bookmarklet.js
@@ -0,0 +1,26 @@
+javascript:(function() {
+    /* level of zoom on map, change this value for more or less zoom. */
+    /* var zoomlevel = x.xx */
+    /* zoom in */
+    var html = document.documentElement;
+    body.scrollTop += 30;
+    /* get current url */
+    var currentUrl = window.location.href;
+    /* iterate url, look for zoom(z) & separator(,) */
+    for (var letter = 0; letter < currentUrl.length; letter++) {
+        if (currentUrl.charAt(letter) == "z") {
+            /* save position of z */
+            var posz = letter;    }
+        else if (currentUrl.charAt(letter) == ",") {
+            /* save position of , */
+            var poscom = letter;    }
+        /* check if "z" and "," found, create new url */
+        else if (currentUrl.charAt(posz) == "z" && currentUrl.charAt(poscom) == ",") {
+            /* creation of new url, remove old zoomlevel and added new specified zoom level */
+            /* replace number with zoomlevel variable */
+            var newUrl = currentUrl.slice(0, poscom) + ",6" + currentUrl.slice(posz, currentUrl.length);
+            /* load new url */
+            window.location.href = newUrl;
+        }
+    }
+})()
